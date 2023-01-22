e33ecd32c6fad64e242a6b67ef9697089a61146b
raelix <raelix@hotmail.it>
2023-01-22 20:23:34 +0100

    updated default vscode path

--- raelix-cluster-v2/main.tf
+++ raelix-cluster-v2/main.tf
@@ -88,7 +88,7 @@ resource "coder_app" "code-server" {
   slug         = "code-server"
   display_name = "code-server"
   icon         = "/icon/code.svg"
-  url          = "http://localhost:13337?folder=/home/coder"
+  url          = "http://localhost:13337?folder=/home/coder/raelix-cluster-v2"
   subdomain    = false
   share        = "owner"
 
